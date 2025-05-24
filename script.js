fb46cb6681e877a6b87b6e227b199c2467ff6ec4
Mohitjangirworks <mohitjangidworks@gmail.com>
2025-05-25 00:05:59 +0530

    test0.5

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  emailjs.sendForm('yooGijwizYzmlHs7a', 'template_x6obkjm', '#contact-form')
-    .then(function(response) {
-      alert('Message sent!');
-    }, function(error) {
-      alert('FAILED... ' + JSON.stringify(error));
-    });
-});
